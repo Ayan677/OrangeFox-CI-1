9590015ec8605b61051e08916053482bb311d576
ShazuxD <116494974+ShazuxD@users.noreply.github.com>
2023-04-29 16:15:29 +0530

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -32,45 +32,11 @@ FILENAME=$(echo $OUTPUT)
 if [ -z "$TIMEOUT" ];then
     TIMEOUT=20160
 fi
+curl --upload-file Orange*.zip https://free.keep.sh
+curl --upload-file Orange*.img https://free.keep.sh
 
-# Upload to WeTransfer
-# NOTE: the current Docker Image, "registry.gitlab.com/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
-
-# Mirror to oshi.at
-curl -T $FILENAME https://oshi.at/${FILENAME}/${TIMEOUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
-
-DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
-MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
-
-# Show the Download Link
-echo "=============================================="
-echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
-echo "=============================================="
-
-DATE_L=$(date +%d\ %B\ %Y)
-DATE_S=$(date +"%T")
-
-# Send the Message on Telegram
-echo -e \
-"
-🦊 OrangeFox Recovery CI
-
-✅ Build Completed Successfully!
-
-📱 Device: "${DEVICE}"
-🖥 Build System: "${FOX_BRANCH}"
-⬇️ Download Link: <a href=\"${DL_LINK}\">Here</a>
-📅 Date: "$(date +%d\ %B\ %Y)"
-⏱ Time: "$(date +%T)"
-" > tg.html
-
-TG_TEXT=$(< tg.html)
-
-telegram_message "$TG_TEXT"
-
-echo " "
-
-# Exit
-exit 0
+wget https://github.com/Sushrut1101/GoFile-Upload/raw/master/upload.sh
+chmod +x upload.sh
+sudo apt install jq -y
+./upload.sh Orange*.zip
+./upload.sh Orange*.img
